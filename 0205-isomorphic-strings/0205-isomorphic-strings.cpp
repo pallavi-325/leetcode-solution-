0fc373474d864bc2e1eab4a6844528494cdc2337
Pallavi Chaudhary <150135762+pallavi-325@users.noreply.github.com>
2025-09-06 19:25:59 +0530

    Time: 1 ms (59.24%), Space: 9.3 MB (81.01%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+        map<char , char > mp1;
+        map<char,char> mp2;
+
+        for(int i=0; i<s.size(); i++){
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if(mp1.count(c1) && mp1[c1] != c2) return false;
+            if(mp2.count(c2) && mp2[c2] != c1) return false;
+
+            mp1[c1] = c2;
+            mp2[c2] = c1;
+        }
+
+        return true;
+    }
+};
