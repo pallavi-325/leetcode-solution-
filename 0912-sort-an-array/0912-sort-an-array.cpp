27b40c4fe360389918e5fd54f4ab5d6c588a566c
Pallavi Chaudhary <150135762+pallavi-325@users.noreply.github.com>
2026-03-10 10:35:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums;
+    vector<int> sortArray(vector<int>& a) {
+        int n = a.size();
+
+        for(int i = 0; i < n-1; i++){
+            int ind = i;
+            for(int j = i+1; j < n; j++){
+                if(a[j] < a[ind])
+                    ind = j;
+            }
+            swap(a[i], a[ind]);
+        }
+        return a;
     }
 };
